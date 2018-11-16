adea835816eccea21d7f6cb6f8b1cbc3aaddbe49
Geoffroy LAUGER <lauger.geoffroy@gmail.com>
2018-11-16 17:29:42 +0100

    Set up ramdom point

--- src/snake.js
+++ src/snake.js
@@ -15,12 +15,35 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 const image = document.getElementById('source');
+const button = document.getElementById("refresh");
 
-ctx.drawImage(image, canvas.width/2 - 200, canvas.height/2 - 100, 400, 200);
-ctx.fillRect(840 , 700, 180, 50);
-ctx.font = "25px Courier new";
-ctx.fillStyle = "white";
-ctx.fillText("Start Game", 845, 730);
+let refresh = function() {
 
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  
+  let x = Math.floor(Math.random()*canvas.width - 10);
+  let y = Math.floor(Math.random()*canvas.height - 10);
+  
+  ctx.beginPath(); 
+  ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+  ctx.fillStyle = "#003d99";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.fillStyle ="#FF0000"
+  ctx.fillRect(canvas.width/2, canvas.height/2, 25, 25);
+
+  ctx.strokeStyle = "#000";
+  ctx.lineWidth = 5;
+  ctx.strokeRect(canvas.width/2, canvas.height/2, 25, 25);
+
+  ctx.drawImage(image, canvas.width/2 - 200, canvas.height/2 - 100, 400, 200);
+  ctx.fillRect(840 , 700, 180, 50);
+  ctx.font = "25px Courier new";
+  ctx.fillStyle = "white";
+  ctx.fillText("Start Game", 845, 730);
+  }
+  refresh();
+  button.addEventListener("click", refresh, false);
 }
 
